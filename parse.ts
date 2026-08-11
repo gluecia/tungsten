bc4681afc29393486e2830becae8307d3c023ae8
luciacover <llcsaturnaliam@gmail.com>
2026-08-11 14:05:59 -0400

    changed linting rules + formatted

--- parse.ts
+++ parse.ts
@@ -1,231 +1,232 @@
 interface Expr {
-    render(): string;
+  render(): string;
 }
 
 export class SurroundExpr implements Expr {
-    readonly surroundChar: string;
-    readonly inner: Expr;
+  readonly surroundChar: string;
+  readonly inner: Expr;
 
-    constructor(inner: Expr, surroundChar: string) {
-        this.inner = inner;
-        this.surroundChar = surroundChar;
-    }
+  constructor(inner: Expr, surroundChar: string) {
+    this.inner = inner;
+    this.surroundChar = surroundChar;
+  }
 
-    render(): string {
-        return `${this.surroundChar}${this.inner.render()}${this.surroundChar}`;
-    }
+  render(): string {
+    return `${this.surroundChar}${this.inner.render()}${this.surroundChar}`;
+  }
 }
 
 export class StringExpr implements Expr {
-    readonly inner: string;
+  readonly inner: string;
 
-    constructor(inner: string) {
-        this.inner = inner;
-    }
+  constructor(inner: string) {
+    this.inner = inner;
+  }
 
-    render(): string {
-        return this.inner;
-    }
+  render(): string {
+    return this.inner;
+  }
 }
 
 export class CodeExpr implements Expr {
-    readonly language: string;
-    readonly inner: string;
+  readonly language: string;
+  readonly inner: string;
 
-    constructor(inner: string, language: string) {
-        this.inner = inner;
-        this.language = language;
-    }
+  constructor(inner: string, language: string) {
+    this.inner = inner;
+    this.language = language;
+  }
 
-    render(): string {
-        return `\`\`\`${this.language}\n${this.inner}\n\`\`\``;
-    }
+  render(): string {
+    return `\`\`\`${this.language}\n${this.inner}\n\`\`\``;
+  }
 }
 
 export class HeadingExpr implements Expr {
-    readonly level: number;
-    readonly inner: Expr;
+  readonly level: number;
+  readonly inner: Expr;
 
-    constructor(inner: Expr, level: number) {
-        this.inner = inner;
-        this.level = level;
+  constructor(inner: Expr, level: number) {
+    this.inner = inner;
+    this.level = level;
+  }
+
+  render(): string {
+    let output = "";
+    for (let i = 0; i < this.level; i++) {
+      output += "#";
     }
 
-    render(): string {
-        let output = "";
-        for (let i = 0; i < this.level; i++) {
-            output += "#";
-        }
-
-        return output + this.inner.render();
-    }
+    return output + this.inner.render();
+  }
 }
 
 export class LinkExpr implements Expr {
-    readonly title: string;
-    readonly link: string;
+  readonly title: string;
+  readonly link: string;
 
-    constructor(title: string, link: string) {
-        this.title = title;
-        this.link = link;
-    }
+  constructor(title: string, link: string) {
+    this.title = title;
+    this.link = link;
+  }
 
-    render(): string {
-        return `[${this.title}](${this.link})`;
-    }
+  render(): string {
+    return `[${this.title}](${this.link})`;
+  }
 }
 
 class RootExpr implements Expr {
-    readonly inner: Expr[];
+  readonly inner: Expr[];
 
-    constructor(inner: Expr[]) {
-        this.inner = inner;
-    }
+  constructor(inner: Expr[]) {
+    this.inner = inner;
+  }
 
-    render(): string {
-        return this.inner.reduce((acc, expr) => (acc += expr.render()), "");
-    }
+  render(): string {
+    return this.inner.reduce((acc, expr) => (acc += expr.render()), "");
+  }
 }
 
 export class Scanner {
-    readonly source: string;
-    private current: number = 0;
-    private start: number = 0;
-    private exprList: Expr[] = [];
+  readonly source: string;
+  private current: number = 0;
+  private start: number = 0;
+  private exprList: Expr[] = [];
 
-    constructor(source: string) {
-        this.source = source;
+
+  constructor(source: string) {
+    this.source = source;
+  }
+
+  scan(): RootExpr {
+    while (!this.isAtEnd()) {
+      this.exprList.push(this.scanToken());
     }
 
-    scan(): RootExpr {
-        while (!this.isAtEnd()) {
-            this.exprList.push(this.scanToken());
-        }
+    return new RootExpr(this.exprList);
+  }
 
-        return new RootExpr(this.exprList);
+  scanToken(): Expr {
+    const c = this.advance();
+
+    switch (c) {
+      case "*":
+        return this.heading();
+      case "/":
+        return this.surroundExpr("/", "*");
+      case "%":
+        return this.surroundExpr("%", "**");
+      case "[":
+        return this.link();
+      default:
+        return this.string();
+    }
+  }
+
+  private surroundExpr(
+    surroundCharacter: string,
+    replaceCharacter: string,
+  ): SurroundExpr {
+    this.start = this.current;
+    while (this.peek() !== surroundCharacter && !this.isAtEnd()) {
+      this.advance();
     }
 
-    scanToken(): Expr {
-        const c = this.advance();
+    this.advance();
 
-        switch (c) {
-            case "*":
-                return this.heading();
-            case "/":
-                return this.surroundExpr("/", "*");
-            case "%":
-                return this.surroundExpr("%", "**");
-            case "[":
-                return this.link();
-            default:
-                return this.string();
-        }
+    const scanner = new Scanner(
+      this.source.substring(this.start, this.current - 1),
+    );
+    const inner = scanner.scan();
+    return new SurroundExpr(inner, replaceCharacter);
+  }
+
+  /**
+   * Parses a heading.
+   * @returns A HeadingExpr containing the heading level and the text.
+   */
+  private heading(): HeadingExpr {
+    let headingLevel = 1;
+    while (this.peek() === "*" && !this.isAtEnd()) {
+      this.advance();
+      headingLevel += 1;
     }
 
-    private surroundExpr(
-        surroundCharacter: string,
-        replaceCharacter: string,
-    ): SurroundExpr {
-        this.start = this.current;
-        while (this.peek() !== surroundCharacter && !this.isAtEnd()) {
-            this.advance();
-        }
+    this.start = this.current;
 
+    while (this.peek() !== "\n" && !this.isAtEnd()) {
+      this.advance();
+    }
+
+    this.advance();
+
+    const scanner = new Scanner(
+      this.source.substring(this.start, this.current),
+    );
+    const inner = scanner.scan();
+    return new HeadingExpr(inner, headingLevel);
+  }
+
+  /**
+   * Parses a string of characters.
+   * @returns A StringExpr containing all characters in the string.
+   */
+  private string(): StringExpr {
+    const specialCharacters = /[%\/\*\[]/;
+
+    this.start = this.current - 1;
+    while (!specialCharacters.test(this.peek()) && !this.isAtEnd()) {
+      const c = this.advance();
+      if (c === "\\" && !this.isAtEnd()) {
         this.advance();
+      }
+    }
+    return new StringExpr(this.source.substring(this.start, this.current));
+  }
 
-        const scanner = new Scanner(
-            this.source.substring(this.start, this.current - 1),
-        );
-        const inner = scanner.scan();
-        return new SurroundExpr(inner, replaceCharacter);
+  private link(): Expr {
+    const start = this.current;
+    while (this.peek() !== "|" && !this.isAtEnd()) {
+      this.advance();
     }
 
-    /**
-     * Parses a heading.
-     * @returns A HeadingExpr containing the heading level and the text.
-     */
-    private heading(): HeadingExpr {
-        let headingLevel = 1;
-        while (this.peek() === "*" && !this.isAtEnd()) {
-            this.advance();
-            headingLevel += 1;
-        }
-
-        this.start = this.current;
-
-        while (this.peek() !== "\n" && !this.isAtEnd()) {
-            this.advance();
-        }
-
-        this.advance();
-
-        const scanner = new Scanner(
-            this.source.substring(this.start, this.current),
-        );
-        const inner = scanner.scan();
-        return new HeadingExpr(inner, headingLevel);
+    if (this.isAtEnd()) {
+      return new StringExpr(
+        this.source.substring(start - 1, this.current),
+      );
     }
 
-    /**
-     * Parses a string of characters.
-     * @returns A StringExpr containing all characters in the string.
-     */
-    private string(): StringExpr {
-        const specialCharacters = /[%\/\*\[]/;
+    const title = this.source.substring(start, this.current);
+    this.advance();
 
-        this.start = this.current - 1;
-        while (!specialCharacters.test(this.peek()) && !this.isAtEnd()) {
-            const c = this.advance();
-            if (c === "\\" && !this.isAtEnd()) {
-                this.advance();
-            }
-        }
-        return new StringExpr(this.source.substring(this.start, this.current));
+    const start2 = this.current;
+    while (this.peek() !== "]" && !this.isAtEnd()) {
+      this.advance();
     }
 
-    private link(): Expr {
-        const start = this.current;
-        while (this.peek() !== "|" && !this.isAtEnd()) {
-            this.advance();
-        }
-
-        if (this.isAtEnd()) {
-            return new StringExpr(
-                this.source.substring(start - 1, this.current),
-            );
-        }
-
-        const title = this.source.substring(start, this.current);
-        this.advance();
-
-        const start2 = this.current;
-        while (this.peek() !== "]" && !this.isAtEnd()) {
-            this.advance();
-        }
-
-        if (this.isAtEnd()) {
-            return new StringExpr(
-                this.source.substring(start - 1, this.current),
-            );
-        }
-
-        const link = this.source.substring(start2, this.current);
-        this.advance();
-
-        return new LinkExpr(title, link);
+    if (this.isAtEnd()) {
+      return new StringExpr(
+        this.source.substring(start - 1, this.current),
+      );
     }
 
-    private isAtEnd(): boolean {
-        return this.current >= this.source.length;
-    }
+    const link = this.source.substring(start2, this.current);
+    this.advance();
 
-    private advance(): string {
-        return this.source.charAt(this.current++);
-    }
+    return new LinkExpr(title, link);
+  }
 
-    private peek(): string {
-        if (this.isAtEnd()) return "\0";
+  private isAtEnd(): boolean {
+    return this.current >= this.source.length;
+  }
 
-        return this.source.charAt(this.current);
-    }
+  private advance(): string {
+    return this.source.charAt(this.current++);
+  }
+
+  private peek(): string {
+    if (this.isAtEnd()) return "\0";
+
+    return this.source.charAt(this.current);
+  }
 }
--- parse_test.ts
+++ parse_test.ts
@@ -1,76 +1,76 @@
 import { LinkExpr, Scanner } from "./parse.ts";
-import { assertEquals } from "jsr:@std/assert@1";
+import { assertEquals } from "jsr:@std/assert";
 
 Deno.test("Text", async (t) => {
-    await t.step("Single Line", () => {
-        const scanner = new Scanner("Test!");
-        assertEquals(scanner.scan().render(), "Test!");
-    });
+  await t.step("Single Line", () => {
+    const scanner = new Scanner("Test!");
+    assertEquals(scanner.scan().render(), "Test!");
+  });
 
-    await t.step("Multiple Lines", () => {
-        const scanner = new Scanner("Test!\nTest again!");
-        assertEquals(scanner.scan().render(), "Test!\nTest again!");
-    });
+  await t.step("Multiple Lines", () => {
+    const scanner = new Scanner("Test!\nTest again!");
+    assertEquals(scanner.scan().render(), "Test!\nTest again!");
+  });
 
-    await t.step("Markup", () => {
-        const scanner = new Scanner(
-            "* H1\nHello! This is /italic/ and this is %bold%!",
-        );
-        assertEquals(
-            scanner.scan().render(),
-            "# H1\nHello! This is *italic* and this is **bold**!",
-        );
-    });
+  await t.step("Markup", () => {
+    const scanner = new Scanner(
+      "* H1\nHello! This is /italic/ and this is %bold%!",
+    );
+    assertEquals(
+      scanner.scan().render(),
+      "# H1\nHello! This is *italic* and this is **bold**!",
+    );
+  });
 });
 
 Deno.test("Heading", async (t) => {
-    await t.step("H1", () => {
-        const scanner = new Scanner("* H1");
-        assertEquals(scanner.scan().render(), "# H1");
-    });
+  await t.step("H1", () => {
+    const scanner = new Scanner("* H1");
+    assertEquals(scanner.scan().render(), "# H1");
+  });
 
-    await t.step("H2", () => {
-        const scanner = new Scanner("** H2");
-        assertEquals(scanner.scan().render(), "## H2");
-    });
+  await t.step("H2", () => {
+    const scanner = new Scanner("** H2");
+    assertEquals(scanner.scan().render(), "## H2");
+  });
 
-    await t.step("H1, Text, H2", () => {
-        const scanner = new Scanner("* H1\nHello!\n** H2");
-        assertEquals(scanner.scan().render(), "# H1\nHello!\n## H2");
-    });
+  await t.step("H1, Text, H2", () => {
+    const scanner = new Scanner("* H1\nHello!\n** H2");
+    assertEquals(scanner.scan().render(), "# H1\nHello!\n## H2");
+  });
 
-    await t.step("H1, H2, H3", () => {
-        const scanner = new Scanner("* H1\n** H2\n*** H3");
-        assertEquals(scanner.scan().render(), "# H1\n## H2\n### H3");
-    });
+  await t.step("H1, H2, H3", () => {
+    const scanner = new Scanner("* H1\n** H2\n*** H3");
+    assertEquals(scanner.scan().render(), "# H1\n## H2\n### H3");
+  });
 
-    await t.step("New Line", () => {
-        const scanner = new Scanner("Test!\n* H1");
-        assertEquals(scanner.scan().render(), "Test!\n# H1");
-    });
+  await t.step("New Line", () => {
+    const scanner = new Scanner("Test!\n* H1");
+    assertEquals(scanner.scan().render(), "Test!\n# H1");
+  });
 
-    await t.step("Markup", () => {
-        const scanner = new Scanner("* /italics/ %bold%");
-        assertEquals(scanner.scan().render(), "# *italics* **bold**");
-    });
+  await t.step("Markup", () => {
+    const scanner = new Scanner("* /italics/ %bold%");
+    assertEquals(scanner.scan().render(), "# *italics* **bold**");
+  });
 });
 
 Deno.test("Links", async (t) => {
-    await t.step("Render", () => {
-        const expr = new LinkExpr("test", "test.com");
-        assertEquals(expr.render(), "[test](test.com)");
-    });
+  await t.step("Render", () => {
+    const expr = new LinkExpr("test", "test.com");
+    assertEquals(expr.render(), "[test](test.com)");
+  });
 
-    await t.step("Basic Link", () => {
-        const scanner = new Scanner("[test|test.com]");
-        assertEquals(scanner.scan().render(), "[test](test.com)");
-    });
+  await t.step("Basic Link", () => {
+    const scanner = new Scanner("[test|test.com]");
+    assertEquals(scanner.scan().render(), "[test](test.com)");
+  });
 
-    await t.step("Complex Link", () => {
-        const scanner = new Scanner("This is a [test link|test.com/testing]!");
-        assertEquals(
-            scanner.scan().render(),
-            "This is a [test link](test.com/testing)!",
-        );
-    });
+  await t.step("Complex Link", () => {
+    const scanner = new Scanner("This is a [test link|test.com/testing]!");
+    assertEquals(
+      scanner.scan().render(),
+      "This is a [test link](test.com/testing)!",
+    );
+  });
 });
