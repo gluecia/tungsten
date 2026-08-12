bb322f1c4d11c7a4266a3ea2d36fe391c3be7dbc
luciacover <llcsaturnaliam@gmail.com>
2026-08-11 22:41:04 -0400

    added cli

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { Scanner } from "./parse.ts";
+
+const args = Deno.args;
+
+function runFile(filename: string) {
+  const contents = Deno.readTextFileSync(filename);
+  const fileRoot = filename.split(".tg")[0];
+
+  const scanner = new Scanner(contents);
+
+  Deno.writeTextFileSync(`${fileRoot}.md`, scanner.scan().render());
+}
+
+if (args.length === 1) {
+  runFile(args[0]);
+} else if (args === 0) {
+  repl();
+} else {
+  console.error("Usage: deno run parse <filename>")
+}
